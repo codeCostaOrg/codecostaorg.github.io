74c356752fadcff7ffd61010862edb8342667d8e
luan-dev <progluan@gmail.com>
2025-05-09 21:28:08 -0400

    feat: add WaveBorder component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import logo from "/logo.png";
 import "./App.css";
+import WaveBorder from "./components/WaveBorder";
 
 function App() {
   return (
     <>
       <img src={logo} className="logo" alt="codeCosta logo" />
       <h1>Welcome</h1>
+      <WaveBorder lowerColor="#FFFFFF" />
     </>
   );
 }
