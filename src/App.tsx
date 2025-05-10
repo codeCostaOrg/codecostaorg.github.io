64da0dba30a115df522aa2d6943daf565cfda93a
luan-dev <progluan@gmail.com>
2025-05-09 20:22:59 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from "/logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={logo} className="logo" alt="codeCosta logo" />
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default App;
